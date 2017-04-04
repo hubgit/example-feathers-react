46c2acbeb063792ad3745c03aadc628239c571b4
Alf Eaton <eaton.alf@gmail.com>
2017-04-05 00:12:00 +0100

    Add authentication notes

--- packages/server/src/authentication.js
+++ packages/server/src/authentication.js
@@ -6,11 +6,13 @@ module.exports = function () {
   const app = this
 
   // TODO: store the secret in env
+  // NOTE: this creates the 'authentication' service
   app.configure(authentication({ secret: 'change-this-secret' }))
   app.configure(local())
   app.configure(jwt())
 
   // Add the authentication hook
+  // See https://github.com/feathersjs/feathers-authentication-local/issues/4 for why this is needed
   app.service('authentication').hooks({
     before: {
       create: authentication.hooks.authenticate(['local', 'jwt'])
