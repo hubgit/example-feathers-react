0dc53b1e6a57527298676d270e74f74694993d26
Alf Eaton <hubgit@users.noreply.github.com>
2017-05-19 14:57:52 +0100

    Update PrivateRoute.jsx

--- packages/client/src/components/PrivateRoute.jsx
+++ packages/client/src/components/PrivateRoute.jsx
@@ -4,6 +4,8 @@ import { Redirect, Route, withRouter } from 'react-router-dom'
 
 const PrivateRoute = ({ currentUser, component, ...rest }) => (
   <Route { ...rest } render={ props => {
+    // TODO: display error message instead of redirecting to signin where appropriate (e.g. if the server is down)  
+      
     if (!currentUser) {
       return <Redirect to={{ pathname: '/signin', state: { from: rest.location } }}/>
     }
